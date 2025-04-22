91383210e9ce2c6a75f57ea7be674f63332d1e50
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-04-22 17:59:24 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="fixed top-6 left-6">
+          <Header />
+        </div>
         {children}
       </body>
     </html>
