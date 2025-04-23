062cf12b0fc17718e9f0712ce3fc6b0002465bf9
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-04-23 11:19:52 +0900

    Update footer.tsx

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="text-center">
+      <small className="copyright text-white font-bold text-xl">
+        &copy; hii dev. 2025
+      </small>
+    </div>
+  );
+}
