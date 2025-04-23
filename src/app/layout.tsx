7fbf9a4dba460afc3ed8f671a59d448618c88bfe
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-04-23 09:25:43 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="fixed top-6 left-6">
+        <div className="fixed top-6 left-6 z-50">
           <Header />
         </div>
         {children}
