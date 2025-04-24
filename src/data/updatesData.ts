578249e05441fb54740f01a4cf369fb935de31d6
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-04-25 01:05:33 +0900

    Create updatesData.ts

---
+++ src/data/updatesData.ts
@@ -0,0 +1,11 @@
+export const data = [
+  {
+    id: "20250425-001",
+    title: "hii-dev. New Release!!",
+    date: "2025.04.25",
+    description: `　2024年に制作した"hii-site."を今まで継続的に改修して運用していましたが、シンプルなデザインのポートフォリオサイトに変更したくなったので新たに制作しました。
+  　今後はこちらの"hii-dev."をポートフォリオサイトとして公開します。`,
+    techStack: "Next.js / TypeScript / TailwindCSS-v4",
+    githubLink: "https://github.com/CA01971020",
+  },
+];
