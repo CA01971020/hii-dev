0e6cb46e27ea4eed6f575cb5ffc71caa15f6cf91
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-04-25 00:01:28 +0900

    Update page.tsx

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,6 +2,10 @@ export default function Portfolios() {
   return (
     <div className=" relative top-32">
       <h1 className="relative text-center">Portfolios</h1>
+
+      <h4 className="relative text-center mt-24 text-3xl text-white">
+        Coming Soon...
+      </h4>
     </div>
   );
 }
