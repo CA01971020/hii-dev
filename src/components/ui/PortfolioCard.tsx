3133387ae369968d560747bce6bc9093848ebafb
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-05-07 12:11:42 +0900

    Update PortfolioCard.tsx

--- src/components/ui/PortfolioCard.tsx
+++ src/components/ui/PortfolioCard.tsx
@@ -4,20 +4,32 @@ import React from "react";
 
 type PortfolioCardProps = {
   title: string;
+  period?: string;
   url?: string;
+  imageUrl?: string;
   description: string;
-  techStackTag?: string;
+  techStackTag?: string[];
 };
 
 export const PortfolioCard: React.FC<PortfolioCardProps> = ({
   title,
+  period,
   url,
+  imageUrl,
   description,
   techStackTag,
 }) => {
   return (
     <div className="duration-200 hover:scale-103 relative mt-16 bg-white pt-6 pb-5 pr-8 pl-8 ml-8 mr-8 rounded-2xl shadow-lg">
-      <div className=" bg-black rounded-md h-60"></div>
+      {imageUrl ? (
+        <img
+          src={imageUrl}
+          alt={title}
+          className="rounded-md h-60 w-full object-cover"
+        />
+      ) : (
+        <div className="bg-black rounded-md h-60 w-full" />
+      )}
       <h2 className="mt-4">{title}</h2>
       {url && (
         <a
@@ -29,8 +41,20 @@ export const PortfolioCard: React.FC<PortfolioCardProps> = ({
           {url}
         </a>
       )}
+      <p className="mt-4">開発期間：{period}</p>
       <p className="mt-4">{description}</p>
-      <p className="mt-4 bg-gray-300">{techStackTag}</p>
+      {techStackTag && (
+        <div className="mt-4 flex flex-wrap gap-2 mb-2">
+          {techStackTag.map((tag, index) => (
+            <span
+              key={index}
+              className="bg-gray-300 text-sm text-gray-800 px-2 py-1 rounded-xl"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
