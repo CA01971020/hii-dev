ea9c798cc7543b3a38c4ffdd711c3e676ce457e0
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-05-09 14:57:54 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import Menu from "./menu";
 
 export default function Header() {
   return (
-    <div className="flex items-center pt-1 pb-1 bg-turquoise/30 backdrop-blur-md">
+    <div className="flex items-center pt-1 pb-1 bg-turquoise/30 backdrop-blur-md border-b-1 border-b-gray/50">
       <a
         href="https://github.com/CA01971020"
         target="_blank"
